eecab8cdd6bd80437a1fc01d78b6d8c633bd143d
sudhakar961612 <s3xhdhh@gmail.com>
2024-05-18 17:10:02 +0530

    add a product

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,19 +1,10 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuLabel,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+
 const Product = () => {
   const [data, setD] = useState([]);
-  const [type, setType] = useState(null);
+
   const [search, setS] = useState("");
   const getProducts = async () => {
     const res = await axios.get("https://fakestoreapi.com/products");
@@ -24,18 +15,18 @@ const Product = () => {
   }, []);
   return (
     <div>
-      <h1 className="text-3xl text-gray-400 my-5">Products Listing</h1>
+      <h1 className='text-3xl text-gray-400 my-5'>Products Listing</h1>
 
       <input
-        className="py-2 px-4 rounded-xl flex-1 mb-3 w-1/2 border border-gray-300 "
+        className='py-2 px-4 rounded-xl flex-1 mb-3 w-1/2 border border-gray-300 '
         value={search}
-        placeholder="search by title ex: jacket "
+        placeholder='search by title ex: jacket '
         onChange={(e) => {
           setS(e.target.value);
         }}
       />
-      <div className="flex-flex-col justify-center">
-        <div className="grid md:grid-cols-3 gap-4 border-gray-300 border px-2 py-2 rounded-xl mb-5">
+      <div className='flex-flex-col justify-center'>
+        <div className='grid md:grid-cols-3 gap-4 border-gray-300 border px-2 py-2 rounded-xl mb-5'>
           {data.map((ele: any, index) => {
             console.log(index);
             let tt = ele.title.toLowerCase();
@@ -47,23 +38,15 @@ const Product = () => {
             }
             if (ok) {
               return (
-                <div
-                  key={index}
-                  className="flex flex-col items-center justify-center order-gray-300 border px-2 py-2 rounded-xl gap-4"
-                >
-                  <div className="h-[200px] w-[200px] overflow-hidden">
-                    <img src={ele.image} className="object-cover" />
+                <div key={index} className='flex flex-col items-center justify-center order-gray-300 border px-2 py-2 rounded-xl gap-4'>
+                  <div className='h-[200px] w-[200px] overflow-hidden'>
+                    <img src={ele.image} className='object-cover' />
                   </div>
-                  <div className="flex flex-col gap-4">
-                    <h1 className="text-xl">{ele.title}</h1>
-                    <p className="text-gray-400">
-                      {ele.description.slice(0, 45)}...
-                    </p>
+                  <div className='flex flex-col gap-4'>
+                    <h1 className='text-xl'>{ele.title}</h1>
+                    <p className='text-gray-400'>{ele.description.slice(0, 45)}...</p>
                     <p>Price: {ele.price}</p>
-                    <Link
-                      to={`/product/${ele.id}`}
-                      className="bg-red-300 border border-gray-500 rounded-xl py-2 px-4"
-                    >
+                    <Link to={`/product/${ele.id}`} className='bg-red-300 border border-gray-500 rounded-xl py-2 px-4'>
                       See More...
                     </Link>
                   </div>
